7896877809500e16e93c41a6724dda19cd399864
BenjaminLawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2024-04-27 14:32:44 +0800

    Created rotational script

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -1,4 +1,9 @@
 extends Sprite2D
 
-func _init():
-	print("Hello, world!")
+var speed = 400
+var angular_speed = PI
+
+func _process(delta):
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
